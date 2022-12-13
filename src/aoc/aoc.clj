48af5f1276d4fc3351b26c0e17ad9129309902b1
oneiromantica <hp@campudus.com>
2022-12-13 15:35:29 +0100

    Add A* to tools

--- src/aoc/aoc.clj
+++ src/aoc/aoc.clj
@@ -1,4 +1,7 @@
-(ns aoc.aoc)
+(ns aoc.aoc
+  (:require [clojure.data.priority-map :as pm]
+            [clojure.pprint :refer [pprint]]
+            [clojure.set :as set]))
 
 (defn spy>> [msg val] (prn msg val) val)
 (defn spy>  [val msg] (spy>> msg val))
@@ -24,3 +27,61 @@
             (scanmap (fn [nxt prv] (if (> nxt prv) nxt prv))
                      2
                      (range 6))))
+
+(defn a*
+  "Shortest path.
+    `h`: node -> node -> num     cost estimation to end
+    `cost`: node -> node -> num  cost to reach neighbour
+    `start`: node
+    `end`: node"
+  [{:keys [h cost start end] :or {cost (fn [a b] (->> (map - a b) (map #(Math/abs %)) (apply +)))
+                                  h    (constantly 1)}}]
+  (fn go
+    ([nodes] (go (assoc-in nodes [start :g] 0)
+                 (assoc (pm/priority-map-by (fn [a b] (- (:f b) (:f a))))
+                        start
+                        (get nodes start))
+                 #{}))
+    ([nodes open closed]     
+     (if-let [[curr] (peek open)]
+       (if (= curr end)
+        {:node curr, :state nodes}
+        (let [border (set/difference (set (get-in nodes [curr :border] #{})) closed)
+              nodes'
+              (reduce (fn [state nxt]
+                        (let [g      (get-in state [curr :g])
+                              h'     (h nxt end)
+                              f      (+ g h')
+                              c      (+ g (cost curr nxt))
+                              new    (not (contains? open nxt))
+                              better (< f (get-in state [nxt :g] Float/POSITIVE_INFINITY))
+                              nxt'   (if (or better new)
+                                       (assoc (get state nxt)
+                                              :g c
+                                              :from curr)
+                                       (get state nxt))]
+                          (assoc state nxt nxt')))
+                      nodes
+                      border)
+              open' (into (pop open) (map (fn [k] [k (get nodes k)]) border))]
+          (recur nodes' open' (conj closed curr))))
+       :no-path-found))))
+
+(defn a*-backtrack
+  ([{:keys [node] :as result}] (a*-backtrack result (list node)))
+  ([{:keys [node state]} path]
+   (if-let [origin (get-in state [node :from])]
+     (recur {:state state, :node origin} (conj path origin))
+     path)))
+
+(comment
+  (let [nodes {[0 0] {:border [[0 1] [1 0]]  :f 3}
+               [0 1] {:border [[0 0] [1 1]]  :f 2}
+               [0 2] {:border [[0 1] [1 2]]  :f 1}
+               [1 0] {:border [[0 0] [1 1]]  :f 2}
+               [1 1] {:border [[1 0] [1 2]]  :f 1}
+               [1 2] {:border [[1 1] [0 2]]  :f 0}}
+        manhattan (fn [a b] (->> (map - a b) (map #(Math/abs %)) (apply +)))
+        find-path (a* {:h manhattan, :cost (constantly 1), :start [0 0], :end [1 2]})]
+    (pprint (find-path nodes))
+    (a*-backtrack (find-path nodes))))
